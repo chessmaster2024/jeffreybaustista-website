1999fd6224dedd02260e51544abeb1693e43578a
chessmaster2024 <joncedricu@gmail.com>
2024-05-27 19:04:25 +0800

    Add files via upload

---
+++ My JS/js.js
@@ -0,0 +1,59 @@
+function changeLink() {
+    document.getElementById("myLink").href = "My CSS/style2.css";
+}
+
+function changeLink2() {
+    document.getElementById("myLink2").href = "My CSS/style2.css";
+}
+
+function changeLink3() {
+    document.getElementById("myLink3").href = "My CSS/style2.css";
+}
+
+function changeLink4() {
+    document.getElementById("myLink4").href = "My CSS/style2.css";
+}
+function changeLink5() {
+    document.getElementById("myLink5").href = "My CSS/style2.css";
+}
+function changeLink6() {
+    document.getElementById("myLink6").href = "My CSS/style2.css";
+}
+
+function changecolorA() {
+document.getElementById("abante").style.color = "yellow";
+}
+
+function changecolorB() {
+document.getElementById("umali").style.color = "skyblue";
+}
+function changecolorC() {
+document.getElementById("plata").style.color = "orange";
+}
+function changecolorD() {
+    document.getElementById("wala").style.color = "pink";
+    }
+function changecolorE() {
+     document.getElementById("ced").style.color = "brown";
+    }
+ function changecolorF() {
+     document.getElementById("jeff").style.color = "red";
+    }
+function changetext10(){
+    document.getElementById("qwe").innerHTML = "WELCOME TO MY WEBSITE!";      
+}
+function changetext20(){
+    document.getElementById("poi").innerHTML = "5 Foods Healthy for our Brain";      
+}
+function changetext30(){
+    document.getElementById("Zz").innerHTML = "5 Foods Healthy for our Eyes";      
+}
+function changetext40(){
+    document.getElementById("Ai").innerHTML = "5 Foods Healthy for our Bones";      
+}
+function changetext50(){
+    document.getElementById("lo").innerHTML = "5 Foods Healthy for our Skin";      
+}
+function changetext60(){
+    document.getElementById("la").innerHTML = "5 Foods Healthy for our Organ";      
+}
